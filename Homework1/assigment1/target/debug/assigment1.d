4aedce93f7f4ae7f2293186e80a5b1ae7558bf71
SpyteByte <83534679+SpyteByte@users.noreply.github.com>
2026-02-07 04:36:28 +0000

    fixed gitignore

--- Homework1/assigment1/target/debug/assigment1.d
+++
@@ -1,1 +0,0 @@
-/workspaces/spring2026/Homework1/assigment1/target/debug/assigment1: /workspaces/spring2026/Homework1/assigment1/src/main.rs
--- Homework1/assigment1/target/debug/deps/assigment1-3b6569177eaf42da.d
+++
@@ -1,5 +0,0 @@
-/workspaces/spring2026/Homework1/assigment1/target/debug/deps/assigment1-3b6569177eaf42da.d: src/main.rs
-
-/workspaces/spring2026/Homework1/assigment1/target/debug/deps/assigment1-3b6569177eaf42da: src/main.rs
-
-src/main.rs:
